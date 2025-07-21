3f71a3e1f66317611e0f9f4c53b6254e3b391591
guthember <guthember@gmail.com>
2025-07-21 19:24:15 +0200

    solved merge conflict

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Swiftful thinking is awesome!");
+Console.WriteLine("This is a new line added to the Program.cs file.");
 Console.WriteLine("this line is added after stashes modification");
