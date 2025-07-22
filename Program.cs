86d12629a5c7c94760b345a6b45acbefb1870669
guthember <guthember@gmail.com>
2025-07-22 07:26:15 +0200

    [feature] use a new class with add a number

--- Program.cs
+++ Program.cs
@@ -4,3 +4,6 @@ Console.WriteLine("This is a new line added to the Program.cs file.");
 Console.WriteLine("this line is added after stashes modification");
 Console.WriteLine("add a line in remote repo");
 Console.WriteLine("This is a new line added to the Program.cs file.");
+
+var solver = new Solver(4);
+Console.WriteLine($"Add 2: {solver.Add(2)}");
