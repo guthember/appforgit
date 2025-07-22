254425401b03a5e28a161604572372062fb13e23
guthember <guthember@gmail.com>
2025-07-22 08:00:00 +0200

    [feature] add some example for calculating

--- Program.cs
+++ Program.cs
@@ -9,3 +9,6 @@ Console.WriteLine("Using the Solver class to perform calculations.");
 var solver = new Solver(4);
 Console.WriteLine($"Add 2: {solver.Add(2)}");
 Console.WriteLine($"Subtract 2: {solver.Subtract(2)}");
+Console.WriteLine($"Multiply by 2: {solver.Multiply(2)}");
+Console.WriteLine($"Divide by 2: {solver.Divide(2)}");
+Console.WriteLine("End of calculations.");  
